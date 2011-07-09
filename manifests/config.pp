415b6e65822c8382fa40238c0640bf566561d3cb
Dave Simons <dave@inuits.be>
2011-07-09 17:42:24 +0200

    cleanup config manifest
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,84 +1,68 @@
 class gitorious::config {
-    file {"/var/www/gitorious/config/database.yml":
-        content => template("gitorious/database.yml.erb"),
-        ensure => present,
-#        require => Exec["git_pull_gitorious"],
-        owner => "git",
-        group => "git",
-    }
+    file {
+		"/var/www/gitorious/config/database.yml":
+			content => template("gitorious/database.yml.erb"),
+			ensure => present,
+			owner => "git",
+			group => "git";
 
-    file {"/var/www/gitorious/config/gitorious.yml":
-        content => template("gitorious/gitorious.yml.erb"),
-        ensure => present,
-        owner => "git",
-        group => "git",
-#        require => Exec["git_pull_gitorious"],
-    }
+		"/var/www/gitorious/config/gitorious.yml":
+			content => template("gitorious/gitorious.yml.erb"),
+			ensure => present,
+			owner => "git",
+			group => "git";
 
-    file {"/var/www/gitorious/config/broker.yml":
-        content => template("gitorious/broker.yml.erb"),
-        ensure => present,
-        owner => "git",
-        group => "git",
-#        require => Exec["git_pull_gitorious"],
-    }
+		"/var/www/gitorious/config/broker.yml":
+			content => template("gitorious/broker.yml.erb"),
+			ensure => present,
+			owner => "git",
+			group => "git";
 
-    file {"/var/www/gitorious/config/environments/production.rb":
-        content => template("gitorious/production.rb.erb"),
-        ensure => present,
-#        require => Exec["git_pull_gitorious"],
-    }
+		"/var/www/gitorious/config/environments/production.rb":
+			content => template("gitorious/production.rb.erb"),
+			ensure => present;
 
-#    exec {"create_db":
-#        command => "rake db:create RAILS_ENV=production",
-#        cwd => "/var/www/gitorious/",
-#        require => File['/var/www/gitorious/config/database.yml', '/var/www/gitorious/config/gitorious.yml'],
-#    }
-
-#    exec {"migrate_db":
-#        command => "rake db:migrate RAILS_ENV=production",
-#        cwd => "/var/www/gitorious/",
-#    }
-
-#    exec {"bootstrap_sphinx":
-#        command => "rake ultrasphinx:bootstrap RAILS_ENV=production",
-#        cwd => "/var/www/gitorious/",
-#        require => [Notify["dependencies_done"], Exec["create_db"]],
-#        notify => Service["httpd"],
-#    }
-
-	exec { "Change_MySQL_server_root_password":
-#		subscribe => [ Package["mysql-server"], Package["mysql-devel"], Service["mysqld"]],
-#		refreshonly => true,
-#		unless => "mysqladmin -uroot -p$mysql_password status",
-		path => "/bin:/usr/bin",
-		command => "mysqladmin -uroot password $mysql_password",
+		"/etc/ld.so.conf.d/gitorious.conf":
+			path => "/etc/ld.so.conf.d/gitorious.conf",
+			source => "puppet:///gitorious/gitorious.conf",
+			owner => "root",
+			group => "root";
 	}
 
-    line { "export_LD_LIBRARY_PATH":
-        file => "/etc/profile",
-        line => "export LD_LIBRARY_PATH=\"/usr/local/lib\"",
-        ensure => present,
-    }
+	exec {
+/*
+		"create_db":
+			command => "rake db:create RAILS_ENV=production",
+			cwd => "/var/www/gitorious/",
+			require => File['/var/www/gitorious/config/database.yml', '/var/www/gitorious/config/gitorious.yml'];
 
-    line { "export_LDFLAGS":
-        file => "/etc/profile",
-        line => "export LDFLAGS=\"-L/usr/local/lib -Wl,-rpath,/usr/local/lib\"",
-        ensure => present,
-    }
+		"migrate_db":
+			command => "rake db:migrate RAILS_ENV=production",
+			cwd => "/var/www/gitorious/";
 
-    file {"/etc/ld.so.conf.d/gitorious.conf":
-        path => "/etc/ld.so.conf.d/gitorious.conf",
-        source => "puppet:///gitorious/gitorious.conf",
-        owner => "root",
-        group => "root",
-    }
+		"bootstrap_sphinx":
+			command => "rake ultrasphinx:bootstrap RAILS_ENV=production",
+			cwd => "/var/www/gitorious/",
+			require => [Notify["dependencies_done"], Exec["create_db"]],
+			notify => Service["httpd"];
+*/
+		"ldconfig":
+			command => "ldconfig",
+			cwd => "/root/",
+			refreshonly => true,
+			require => Package[$package_list],
+			subscribe => [File["/etc/ld.so.conf.d/gitorious.conf"]];
+	}
 
-    exec {"ldconfig":
-        command => "ldconfig",
-        cwd => "/root/",
-        refreshonly => true,
-#        require => Package[$package_list],
-        subscribe => [File["/etc/ld.so.conf.d/gitorious.conf"]],
-    }
+    line {
+		"export_LD_LIBRARY_PATH":
+			file => "/etc/profile",
+			line => "export LD_LIBRARY_PATH=\"/usr/local/lib\"",
+			ensure => present;
+
+		"export_LDFLAGS":
+			file => "/etc/profile",
+			line => "export LDFLAGS=\"-L/usr/local/lib -Wl,-rpath,/usr/local/lib\"",
+			ensure => present;
+	}
 }
