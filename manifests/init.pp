1d53789d618b3203cc8f9bac39db14c43bb9ba2b
Dave Simons <dave@inuits.be>
2011-08-04 16:51:08 +0200

    KISS!
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,23 @@
 import 'classes/*'
 
-class gitorious {
-	include gitorious::config
-	include gitorious::core
-	include gitorious::depends
-	include gitorious::passenger
-	include gitorious::pre
-	include gitorious::repo
-	include gitorious::services
-	include gitorious::user
-	include system::services
-
-	Class['gitorious::pre'] -> Class['gitorious::repo'] -> Class['gitorious::depends'] -> Class['core'] -> Class['gitorious::config'] -> Class['gitorious::user'] -> Class['gitorious::passenger'] -> Class['system::services'] -> Class['gitorious::services']
+class gitorious (
+	$dbuser = 'gitorious',
+	$dbpass = 'gitorious'
+) {
+	class {
+		'gitorious::pre':
+			stage => pre;
+		'gitorious::repo':
+			stage => repo;
+		'gitorious::depends':
+			stage => depends;
+		'gitorious::user':
+			stage => core;
+		'gitorious::core':
+			stage => core;
+		'gitorious::config':
+			stage => config;
+		'gitorious::services':
+			stage => services;
+	}
 }
