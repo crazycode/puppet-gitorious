4e68f06712c2afb37f4f1f2b6152b6c4959b296c
Dave Simons <dave@inuits.be>
2011-08-16 09:30:56 +0200

    add some params and stage defs
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,22 +2,29 @@ import 'classes/*'
 
 class gitorious (
 	$dbuser = 'gitorious',
-	$dbpass = 'gitorious'
+	$dbpass = 'gitorious',
+	$webserver = 'apache2',
+	$home = '/usr/share/gitorious',
+	$stages = 'no'
 ) {
-	class {
-		'gitorious::pre':
-			stage => pre;
-		'gitorious::repo':
-			stage => repo;
-		'gitorious::depends':
-			stage => depends;
-		'gitorious::user':
-			stage => core;
-		'gitorious::core':
-			stage => core;
-		'gitorious::config':
-			stage => config;
-		'gitorious::services':
-			stage => services;
+	if $stages != 'yes' {
+		class{'gitorious::pre':} -> class{'gitorious::repo':} -> class{'gitorious::depends':} -> class{'gitorious::user':} -> class{'gitorious::core':} -> class{'gitorious::config':} -> class{'gitorious::services':}
+	} else {
+		class {
+			'gitorious::pre':
+				stage => pre;
+			'gitorious::repo':
+				stage => repo;
+			'gitorious::depends':
+				stage => depends;
+			'gitorious::user':
+				stage => core;
+			'gitorious::core':
+				stage => core;
+			'gitorious::config':
+				stage => config;
+			'gitorious::services':
+				stage => services;
+		}
 	}
 }
