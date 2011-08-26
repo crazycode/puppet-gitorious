1a146e3bbf1e33dab04bb8f78216b1e1bb6ff900
Dave Simons <dave@inuits.be>
2011-08-26 16:59:01 +0200

    cleanup depends
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/depends.pp
+++ manifests/classes/depends.pp
@@ -27,52 +27,6 @@ class gitorious::depends {
 					"libtiff-devel",
 					'ruby-shadow',
 					'sphinx',
-#					'rubygem-rake',
-					'rubygem-SystemTimer',
-					'rubygem-activesupport',
-					'rubygem-activemessaging',
-					'rubygem-activerecord',
-					'rubygem-acts-as-taggable-on',
-					'rubygem-builder',
-					'rubygem-json',
-					'rubygem-capillary',
-					'rubygem-chronic',
-					'rubygem-daemons',
-					'rubygem-diff-lcs',
-					'rubygem-gemcutter',
-#					'rubygem-json_pure',
-#					'rubygem-rubyforge',
-#					'rubygem-echoe',
-					'rubygem-eventmachine',
-					'rubygem-exception_notification',
-					'rubygem-factory_girl',
-					'rubygem-geoip',
-					'rubygem-hodel_3000_compliant_logger',
-					'rubygem-hoe',
-					'rubygem-mime-types',
-					'rubygem-mocha',
-					'rubygem-mysql',
-					'rubygem-oauth',
-					'rubygem-paperclip',
-					'rubygem-proxymachine',
-					'rubygem-rack',
-					'rubygem-rdiscount',
-					'rubygem-redis',
-					'rubygem-redis-namespace',
-					'rubygem-sinatra',
-					'rubygem-vegas',
-					'rubygem-resque',
-					'rubygem-revo-ssl_requirement',
-					'rubygem-riddle',
-					'rubygem-ruby-hmac',
-					'rubygem-ruby-openid',
-					'rubygem-ruby-yadis',
-					'rubygem-shoulda',
-					'rubygem-state_machine',
-					'rubygem-stomp',
-					'rubygem-tuxml',
-					'rubygem-validates_url_format_of',
-					'rubygem-will_paginate',
 					]
 
 	package {
@@ -81,12 +35,6 @@ class gitorious::depends {
 				'yes' => Class['mysql::packages'],
 				'no' => [ Class['mysql::packages'], Yumrepo['inuits-gems'] ],
 			},
-/*
-			before => $gitorious::stages ? {
-				'yes' => undef,
-				'no' => Yumrepo['epel'],
-			},
-*/
 			ensure => installed;
     
 		'oniguruma':
@@ -118,32 +66,17 @@ class gitorious::depends {
 				'*' => 'curl-devel',
 			};
 
-		'json_pure':
-			ensure => '1.5.1',
-			provider => gem;
-
-		'rubyforge':
-			ensure => '2.0.4',
-			provider => gem;
-
-		'echoe':
-			ensure => '3.2',
-			provider => gem;
-
 		'rake':
 			ensure => '0.9.2',
 			before => $gitorious::stages ? {
-				'yes' => undef,
-				'no' => Exec['install modules'],
+				'yes' => Exec['bundle_install'],
+				'no' => Exec['install modules', 'bundle_install'],
 			},
 			provider => gem;
 
-		'rubygem-rake':
+		'bundler':
 			ensure => present,
-			before => $gitorious::stages ? {
-				'yes' => undef,
-				'no' => Exec['migrate_db'],
-			};
+			provider => gem;
 	}
 
 	@package {
@@ -156,14 +89,11 @@ class gitorious::depends {
 	}
 
 	exec {
-		'remove rake-0.9.2':
-			command => 'gem uni rake -v 0.9.2',
-			path => '/bin:/sbin:/usr/bin:/usr/sbin',
-			require => $gitorious::stages ? {
-				'yes' => undef,
-				'no' => Exec['install modules'],
-			},
-			onlyif => 'gem list -l rake|grep "0.9.2"';
+		'bundle_install':
+			command => 'bundle install',
+			cwd => '/usr/share/gitorious',
+			before => File["$gitorious::home"],
+			require => Exec['git_pull_gitorious'];
 
 		"git_pull_gitorious":
 			command => "git clone git://gitorious.org/gitorious/mainline.git gitorious",
@@ -173,6 +103,7 @@ class gitorious::depends {
 				'no' => File["$gitorious::home"],
 				default => undef,
 			},
+			require => Package[$package_list],
 			timeout => "-1";
 	}
 }
