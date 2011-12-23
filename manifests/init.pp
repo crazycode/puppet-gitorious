b6d01d81068d83fd128f2bf1bd2985d8cba231b4
Dave Simons <dave@inuits.be>
2011-12-23 10:10:44 +0100

    remove stages
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,30 +5,8 @@ class gitorious (
 	$dbpass = 'gitorious',
 	$webserver = 'apache2',
 	$home = '/usr/share/gitorious',
-	$stages = 'no',
 	$host
 ) {
-	if $stages != 'yes' {
-		class{'gitorious::pre':} -> class{'gitorious::repo':} -> class{'gitorious::depends':} -> class{'gitorious::user':} -> class{'gitorious::core':} -> class{'gitorious::config':} -> class{'gitorious::services':}
-	} else {
-		class {
-			'gitorious::pre':
-				stage => pre;
-			'gitorious::repo':
-				stage => repo;
-			'gitorious::depends':
-				stage => depends;
-			'gitorious::user':
-				stage => core;
-			'gitorious::core':
-				stage => core;
-			'gitorious::config':
-				stage => config;
-			'gitorious::services':
-				stage => services;
-		}
-	}
-
   class {
     'apache':;
     'mysql':
@@ -36,6 +14,14 @@ class gitorious (
     'passenger':;
     'repos':;
   }
+
+  class{'gitorious::pre':} ->
+  class{'gitorious::repo':} ->
+  class{'gitorious::depends':} ->
+  class{'gitorious::user':} ->
+  class{'gitorious::core':} ->
+  class{'gitorious::config':} ->
+  class{'gitorious::services':}
 
   Exec { path => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin' }
 }
